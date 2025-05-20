539c36a6945f311840ebd8e7ded626a97192dfea
GuilleJimenez0812 <Jimenezguillermo1208@gmail.com>
2025-05-20 15:28:04 -0400

    refactor: :recycle: go back

--- release.config.cjs
+++ release.config.cjs
@@ -1,9 +1,3 @@
 module.exports = {
-  branches: ["main", {name: "next", prerelease: true}, { name: "v*", tagFormat: "v${version}" }],
-  plugins: [
-    "@semantic-release/commit-analyzer",
-    "@semantic-release/release-notes-generator",
-    "@semantic-release/npm",
-    "@semantic-release/github"
-  ]
+  branches: ["main", {name: "next", prerelease: true}],
 }
